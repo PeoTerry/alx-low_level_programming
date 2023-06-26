db06596b741a2c00e7bf0965477298c6a11ee3d5
PeoTerry <motlhaoloaterry@gmail.com>
2023-06-26 09:15:39 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,13 +3,13 @@
 /**
  * _strlen - return thr length of a string 
  * @s: string 
- * Return: length
+ * Return:length
  */
 int _strlen(char *s)
 {
 	int longi = 0;
 
-	while (*s != \'0')
+	while(*s != '\0')
 	}
           
               longi++;
